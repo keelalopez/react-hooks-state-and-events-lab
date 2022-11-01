3dc9e283f269a8717bd68ede4b4abc5302ba1c30
Keila Lopez <keilalofra@gmail.com>
2022-11-01 10:41:16 -0600

    first and last commit. Worked with Martin, Leah, and Calvin

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,39 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import {useState} from "react";
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // const appClass = false ? "App dark" : "App light"
+  const [ mode, setMode ] = useState( true )
+
+  // // arrow syntax, callback function
+  // return (
+  //   <div className={mode ? "App dark" : "App Light"}> //appClass used to be here
+  //     <header>
+  //       <h2>Shopster</h2>
+  //       <button onClick={() => setMode(!mode)}>{!mode ? "App dark" : "App Light"}</button>
+  //     </header>
+  //     <ShoppingList items={itemData} />
+  //   </div>
+  // );
+
+  // modular attempt
+  const handleClick = () => setMode(!mode)
+  const buttonText = !mode ? "Dark mode" : "Light mode"
 
   return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+      <div className={mode ? "App dark" : "App Light"}> //appClass used to be here
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={handleClick}>{buttonText}</button>
+        </header>
+        <ShoppingList items={itemData} />
+      </div>
+    );
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  const handleClick = () => {setInCart(!inCart)}
+  
+  const liClass = inCart ? "in-cart" : "";
+  const buttonClass = inCart ? "remove" : "add"
+  const buttonText = inCart ? "Remove from Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleClick}>{buttonText}</button>
     </li>
   );
 }
